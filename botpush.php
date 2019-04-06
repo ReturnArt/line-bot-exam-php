22d24101143f8004970a1f8f79f58bb7c3b307eb
DESKTOP-RR6KU1P\l2aid <piya149@gmail.com>
2019-04-06 15:06:50 +0700

    sss

--- botpush.php
+++ botpush.php
@@ -6,9 +6,10 @@ require "vendor/autoload.php";
 
 $access_token = 'Kes8azacx5hMOHZTM17UIwqgAxln4Yx/pPTdlMVhPSSdZqI6D8lgWkrmIkab88E87iwsEo2nj2XqrSIUe1MY27tuO8VIzgHpVK7KFnga34JGXUkRlN8+8lNxPVSC8pvs7PYU4pkG7HLqhyC0ohAcnwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '256b5ce16f6cd3331469c4acd005b309';
+$channelSecret = '256b5ce16f6cd3331469c4acd005b309','U8af1fc926817b0bf74f8b4c2167637a9';
 
-$pushID = 'U8af1fc926817b0bf74f8b4c2167637a9';
+// $pushID = 'U8af1fc926817b0bf74f8b4c2167637a9';
+$pushID = array('U8af1fc926817b0bf74f8b4c2167637a9',);
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
