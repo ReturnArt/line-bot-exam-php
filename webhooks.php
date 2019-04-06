860d531ab430380a5a36efc2756a8603700cbb92
DESKTOP-RR6KU1P\l2aid <piya149@gmail.com>
2019-04-06 14:08:04 +0700

    update webhood keep user message

--- webhooks.php
+++ webhooks.php
@@ -13,6 +13,7 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
+
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
@@ -48,4 +49,18 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
+
+// write file
+date_default_timezone_set(“Asia/Bangkok”);
+$date = date(“Y-m-d”);
+$time = date(“H:i:s”);
+$json = file_get_contents(‘php://input’);
+$request = json_decode($json, true);
+$queryText = $request[“queryResult”][“queryText”];
+$userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
+$myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
+$log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
+fwrite($myfile,$log);
+fclose($myfile);
+
 echo "OK";
