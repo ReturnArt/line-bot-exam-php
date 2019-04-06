3b70c40d659bcb47c471e45ad285f737a8e9dafb
DESKTOP-RR6KU1P\l2aid <piya149@gmail.com>
2019-04-06 14:54:09 +0700

    update webhook

--- webhooks.php
+++ webhooks.php
@@ -54,17 +54,17 @@ if (!is_null($events['events'])) {
 date_default_timezone_set("Asia/Bangkok");
 $date = date("Y-m-d");
 $time = date("H:i:s");
-// $json = file_get_contents('php://input');
-// $request = json_decode($json, true);
-// $queryText = $request["queryResult"]["queryText"];
-// $userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
-// $myfile = fopen("log".$date."txt", "a") or die("Unable to open file!");
-// $log = $date."-".$time."\t".$userId."\t".$queryText."\n";
+$json = file_get_contents('php://input');
+$request = json_decode($json, true);
+$queryText = $request["queryResult"]["queryText"];
+$userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
+$myfile = fopen("log".$date."txt", "a") or die("Unable to open file!");
+$log = $date."-".$time."\t".$userId."\t".$queryText."\n";
 // fwrite($myfile,$log);
 // fclose($myfile);
 
 $myfile = fopen("logfile.txt", "a") or die("Unable to open file!");
-$txt = "Add ".$date." ".$time;
+$txt = $log;
 fwrite($myfile, "\n". $txt);
 fclose($myfile);
 
