27c02e4611df53240cc7d935ac9935029f834c08
Piya Naravadee <piya149@gmail.com>
2019-04-08 17:31:24 +0700

    sticker

--- test/botflex.php
+++ test/botflex.php
@@ -14,203 +14,202 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 // text
-$messageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('test text1', 'test text2');
-$response = $bot->pushMessage($pushID, $messageBuilder);
-echo "text : ".$response->getHTTPStatus() . ' ' . $response->getRawBody();
+// $messageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('test text1', 'test text2');
+// $response = $bot->pushMessage($pushID, $messageBuilder);
+// echo "text : ".$response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 // image
-$messageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png');
-$response = $bot->pushMessage($pushID, $messageBuilder);
-echo "image : ".$response->getHTTPStatus() . ' ' . $response->getRawBody();
+// $messageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png');
+// $response = $bot->pushMessage($pushID, $messageBuilder);
+// echo "image : ".$response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 // sticker
 $messageBuilder = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder('1', '2');
 $response = $bot->pushMessage($pushID, $messageBuilder);
 echo "sticker : ".$response->getHTTPStatus() . ' ' . $response->getRawBody();
 
-$response = $bot->pushMessage(
-    $pushID,
-    new RawMessageBuilder(
-        [
-            'type' => 'flex',
-            'altText' => 'Shopping',
-            'contents' => [
-                'type' => 'carousel',
-                'contents' => [
-                    [
-                        'type' => 'bubble',
-                        'hero' => [
-                            'type' => 'image',
-                            'size' => 'full',
-                            'aspectRatio' => '20:13',
-                            'aspectMode' => 'cover',
-                            'url' => 'https://example.com/photo1.png'
-                        ],
-                        'body' => [
-                            'type' => 'box',
-                            'layout' => 'vertical',
-                            'spacing' => 'sm',
-                            'contents' => [
-                                [
-                                    'type' => 'text',
-                                    'text' => 'Arm Chair, White',
-                                    'wrap' => true,
-                                    'weight' => 'bold',
-                                    'size' => 'xl'
-                                ],
-                                [
-                                    'type' => 'box',
-                                    'layout' => 'baseline',
-                                    'contents' => [
-                                        [
-                                            'type' => 'text',
-                                            'text' => '$49',
-                                            'wrap' => true,
-                                            'weight' => 'bold',
-                                            'size' => 'xl',
-                                            'flex' => 0
-                                        ],
-                                        [
-                                            'type' => 'text',
-                                            'text' => '.99',
-                                            'wrap' => true,
-                                            'weight' => 'bold',
-                                            'size' => 'sm',
-                                            'flex' => 0
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ],
-                        'footer' => [
-                            'type' => 'box',
-                            'layout' => 'vertical',
-                            'spacing' => 'sm',
-                            'contents' => [
-                                [
-                                    'type' => 'button',
-                                    'style' => 'primary',
-                                    'action' => [
-                                        'type' => 'uri',
-                                        'label' => 'Add to Cart',
-                                        'uri' => 'https://example.com'
-                                    ]
-                                ],
-                                [
-                                    'type' => 'button',
-                                    'action' => [
-                                        'type' => 'uri',
-                                        'label' => 'Add to wishlist',
-                                        'uri' => 'https://example.com'
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ],
-                    [
-                        'type' => 'bubble',
-                        'hero' => [
-                            'type' => 'image',
-                            'size' => 'full',
-                            'aspectRatio' => '20:13',
-                            'aspectMode' => 'cover',
-                            'url' => 'https://example.com/photo2.png'
-                        ],
-                        'body' => [
-                            'type' => 'box',
-                            'layout' => 'vertical',
-                            'spacing' => 'sm',
-                            'contents' => [
-                                [
-                                    'type' => 'text',
-                                    'text' => 'Metal Desk Lamp',
-                                    'wrap' => true,
-                                    'weight' => 'bold',
-                                    'size' => 'xl'
-                                ],
-                                [
-                                    'type' => 'box',
-                                    'layout' => 'baseline',
-                                    'contents' => [
-                                        [
-                                            'type' => 'text',
-                                            'text' => '$11',
-                                            'wrap' => true,
-                                            'weight' => 'bold',
-                                            'size' => 'xl',
-                                            'flex' => 0
-                                        ],
-                                        [
-                                            'type' => 'text',
-                                            'text' => '.99',
-                                            'wrap' => true,
-                                            'weight' => 'bold',
-                                            'size' => 'sm',
-                                            'flex' => 0
-                                        ]
-                                    ]
-                                ],
-                                [
-                                    'type' => 'text',
-                                    'text' => 'Temporarily out of stock',
-                                    'wrap' => true,
-                                    'size' => 'xxs',
-                                    'margin' => 'md',
-                                    'color' => '#ff5551',
-                                    'flex' => 0
-                                ]
-                            ]
-                        ],
-                        'footer' => [
-                            'type' => 'box',
-                            'layout' => 'vertical',
-                            'spacing' => 'sm',
-                            'contents' => [
-                                [
-                                    'type' => 'button',
-                                    'style' => 'primary',
-                                    'color' => '#aaaaaa',
-                                    'action' => [
-                                        'type' => 'uri',
-                                        'label' => 'Add to Cart',
-                                        'uri' => 'https://example.com'
-                                    ]
-                                ],
-                                [
-                                    'type' => 'button',
-                                    'action' => [
-                                        'type' => 'uri',
-                                        'label' => 'Add to wishlist',
-                                        'uri' => 'https://example.com'
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ],
-                    [
-                        'type' => 'bubble',
-                        'body' => [
-                            'type' => 'box',
-                            'layout' => 'vertical',
-                            'spacing' => 'sm',
-                            'contents' => [
-                                [
-                                    'type' => 'button',
-                                    'flex' => 1,
-                                    'gravity' => 'center',
-                                    'action' => [
-                                        'type' => 'uri',
-                                        'label' => 'See more',
-                                        'uri' => 'https://example.com'
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ]
-    )
-);
-
-echo "flex : ".$response->getHTTPStatus() . ' ' . $response->getRawBody();
+// $response = $bot->pushMessage(
+//     $pushID,
+//     new RawMessageBuilder(
+//         [
+//             'type' => 'flex',
+//             'altText' => 'Shopping',
+//             'contents' => [
+//                 'type' => 'carousel',
+//                 'contents' => [
+//                     [
+//                         'type' => 'bubble',
+//                         'hero' => [
+//                             'type' => 'image',
+//                             'size' => 'full',
+//                             'aspectRatio' => '20:13',
+//                             'aspectMode' => 'cover',
+//                             'url' => 'https://example.com/photo1.png'
+//                         ],
+//                         'body' => [
+//                             'type' => 'box',
+//                             'layout' => 'vertical',
+//                             'spacing' => 'sm',
+//                             'contents' => [
+//                                 [
+//                                     'type' => 'text',
+//                                     'text' => 'Arm Chair, White',
+//                                     'wrap' => true,
+//                                     'weight' => 'bold',
+//                                     'size' => 'xl'
+//                                 ],
+//                                 [
+//                                     'type' => 'box',
+//                                     'layout' => 'baseline',
+//                                     'contents' => [
+//                                         [
+//                                             'type' => 'text',
+//                                             'text' => '$49',
+//                                             'wrap' => true,
+//                                             'weight' => 'bold',
+//                                             'size' => 'xl',
+//                                             'flex' => 0
+//                                         ],
+//                                         [
+//                                             'type' => 'text',
+//                                             'text' => '.99',
+//                                             'wrap' => true,
+//                                             'weight' => 'bold',
+//                                             'size' => 'sm',
+//                                             'flex' => 0
+//                                         ]
+//                                     ]
+//                                 ]
+//                             ]
+//                         ],
+//                         'footer' => [
+//                             'type' => 'box',
+//                             'layout' => 'vertical',
+//                             'spacing' => 'sm',
+//                             'contents' => [
+//                                 [
+//                                     'type' => 'button',
+//                                     'style' => 'primary',
+//                                     'action' => [
+//                                         'type' => 'uri',
+//                                         'label' => 'Add to Cart',
+//                                         'uri' => 'https://example.com'
+//                                     ]
+//                                 ],
+//                                 [
+//                                     'type' => 'button',
+//                                     'action' => [
+//                                         'type' => 'uri',
+//                                         'label' => 'Add to wishlist',
+//                                         'uri' => 'https://example.com'
+//                                     ]
+//                                 ]
+//                             ]
+//                         ]
+//                     ],
+//                     [
+//                         'type' => 'bubble',
+//                         'hero' => [
+//                             'type' => 'image',
+//                             'size' => 'full',
+//                             'aspectRatio' => '20:13',
+//                             'aspectMode' => 'cover',
+//                             'url' => 'https://example.com/photo2.png'
+//                         ],
+//                         'body' => [
+//                             'type' => 'box',
+//                             'layout' => 'vertical',
+//                             'spacing' => 'sm',
+//                             'contents' => [
+//                                 [
+//                                     'type' => 'text',
+//                                     'text' => 'Metal Desk Lamp',
+//                                     'wrap' => true,
+//                                     'weight' => 'bold',
+//                                     'size' => 'xl'
+//                                 ],
+//                                 [
+//                                     'type' => 'box',
+//                                     'layout' => 'baseline',
+//                                     'contents' => [
+//                                         [
+//                                             'type' => 'text',
+//                                             'text' => '$11',
+//                                             'wrap' => true,
+//                                             'weight' => 'bold',
+//                                             'size' => 'xl',
+//                                             'flex' => 0
+//                                         ],
+//                                         [
+//                                             'type' => 'text',
+//                                             'text' => '.99',
+//                                             'wrap' => true,
+//                                             'weight' => 'bold',
+//                                             'size' => 'sm',
+//                                             'flex' => 0
+//                                         ]
+//                                     ]
+//                                 ],
+//                                 [
+//                                     'type' => 'text',
+//                                     'text' => 'Temporarily out of stock',
+//                                     'wrap' => true,
+//                                     'size' => 'xxs',
+//                                     'margin' => 'md',
+//                                     'color' => '#ff5551',
+//                                     'flex' => 0
+//                                 ]
+//                             ]
+//                         ],
+//                         'footer' => [
+//                             'type' => 'box',
+//                             'layout' => 'vertical',
+//                             'spacing' => 'sm',
+//                             'contents' => [
+//                                 [
+//                                     'type' => 'button',
+//                                     'style' => 'primary',
+//                                     'color' => '#aaaaaa',
+//                                     'action' => [
+//                                         'type' => 'uri',
+//                                         'label' => 'Add to Cart',
+//                                         'uri' => 'https://example.com'
+//                                     ]
+//                                 ],
+//                                 [
+//                                     'type' => 'button',
+//                                     'action' => [
+//                                         'type' => 'uri',
+//                                         'label' => 'Add to wishlist',
+//                                         'uri' => 'https://example.com'
+//                                     ]
+//                                 ]
+//                             ]
+//                         ]
+//                     ],
+//                     [
+//                         'type' => 'bubble',
+//                         'body' => [
+//                             'type' => 'box',
+//                             'layout' => 'vertical',
+//                             'spacing' => 'sm',
+//                             'contents' => [
+//                                 [
+//                                     'type' => 'button',
+//                                     'flex' => 1,
+//                                     'gravity' => 'center',
+//                                     'action' => [
+//                                         'type' => 'uri',
+//                                         'label' => 'See more',
+//                                         'uri' => 'https://example.com'
+//                                     ]
+//                                 ]
+//                             ]
+//                         ]
+//                     ]
+//                 ]
+//             ]
+//         ]
+//     )
+// );
+// echo "flex : ".$response->getHTTPStatus() . ' ' . $response->getRawBody();
